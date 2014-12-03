022a9f6a5c10b98fee830edec6da305defbc2c93
Odin Dutton <odindutton@gmail.com>
2014-12-03 12:31:49 +0100

    Make RubyMotion compatible

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,4 @@
 require 'stringio'
 require 'test/unit'
-require File.dirname(__FILE__) + '/../lib/link_header'
+require File.dirname(__FILE__) + '/../lib/link_header/strscan'
+require File.dirname(__FILE__) + '/../lib/link_header/link_header'
